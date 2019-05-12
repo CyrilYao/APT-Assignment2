b7e7cee08a4f8cf2264d4bc54e5a351eec192d86
CyrilYao <48315407+CyrilYao@users.noreply.github.com>
2019-05-12 22:44:24 +1000

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -3,6 +3,7 @@
 #define ASSIGN2_LINKEDLIST_H
 
 #include "Node.h"
+#include <fstream>
 
 class LinkedList {
 public:
@@ -12,6 +13,19 @@ public:
 
 private:
    Node* head;
+};
+public:
+  //Add a node to the back of the linklist
+	void AddTail(Colour colour, Shape shape);
+	void AddTail(Node* node);
+  //Show the head node of linklist
+	Node* Pop();
+  //Print out the tile information of node
+	void PrintContent();
+  //Clean out the linklist
+	void Clear();
+  //Print out the side of linklist
+	int size();
 };
 
 #endif // ASSIGN2_LINKEDLIST_H
